7fcbe2e13897ce9e7a94ee06cbc034a88b1534a0
Steve.K.Y <122028234+Steven-Yabann@users.noreply.github.com>
2026-05-09 12:33:53 +0300

    Added cmd command for adding task

---
+++ CMD/add.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/todo"
+)
+
+func AddTask(todo *todo.Todos, args []string) {
+	// Define add command to add todo item
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	addTitle := addCmd.String("title", "", "Title of new todo item")
+	addTask := addCmd.String("task", "", "Content of new todo item")
+
+	// Parse the arguments
+	addCmd.Parse(args)
+
+	// Check if the todo text was provided
+	if flag.NArg() == 0 {
+		fmt.Println("Error: the --task flag is required for the 'add' subcommand.")
+		os.Exit(1)			
+	}
+
+	todo.AddTask(*addTitle, *addTask)
+	err := todo.Store(GetJsonFile())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// print todo
+	fmt.Println("Todo item added successfully.")
+}
